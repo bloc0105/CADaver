b88dcb7d80d727dacf9112c77e9558c70996cf49
Liech <nicolas_harttig@hotmail.de>
2025-05-01 16:22:46 +0200

    clangformat

--- Library/Source/Library/CADShape/CADShape.h
+++ Library/Source/Library/CADShape/CADShape.h
@@ -17,15 +17,14 @@ namespace Library
         virtual std::string toString() const;
         virtual std::string getType() const;
 
-        
-       std::vector<std::unique_ptr<CADShape>> getChildren() const;
+        std::vector<std::unique_ptr<CADShape>> getChildren() const;
 
-        bool save(const std::string& filename) const;
+        bool                             save(const std::string& filename) const;
         static std::unique_ptr<CADShape> load(const std::string& filename);
 
-        TopoDS_Shape& getData();
+        TopoDS_Shape&       getData();
         const TopoDS_Shape& getData() const;
-        void setData(const TopoDS_Shape&);
+        void                setData(const TopoDS_Shape&);
 
       private:
         std::unique_ptr<TopoDS_Shape> data = nullptr;
